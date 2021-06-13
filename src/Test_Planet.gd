6c9200bce8910354466d511b0f9269f5f0d56a63
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-06-13 14:00:04 +0200

    Planets: Planet generation + growing and shrinking

---
+++ src/Test_Planet.gd
@@ -0,0 +1,33 @@
+extends Spatial
+class_name Planet
+
+# Planet stats
+var planet_weight: float = 1
+
+
+func _ready():
+	update_size()
+
+
+func _process(delta):
+	$PlanetsSurface.rotate_y(0.1 * delta)
+	$Clouds.rotate_y(0.2 * delta)
+
+
+func grow_planet():
+	planet_weight += 0.5 + (planet_weight * 0.01)
+	update_size()
+
+
+func shrink_planet():
+	planet_weight -= 0.5 + (planet_weight * 0.01)
+	update_size()
+
+
+func update_size():
+	if planet_weight > 1:
+		self.scale = Vector3.ONE * (pow(0.75 * planet_weight, 0.3))
+	
+	else:
+		planet_weight = 1
+		self.scale = Vector3.ONE
