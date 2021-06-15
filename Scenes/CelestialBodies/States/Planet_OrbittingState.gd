14d3ae779a683ee1b47e5a409e1016b2d681051d
Grzegorz Puławski <pulawskig@gmail.com>
2021-06-15 21:42:35 +0200

    Fixing orbit removal

--- Scenes/CelestialBodies/States/Planet_OrbittingState.gd
+++ Scenes/CelestialBodies/States/Planet_OrbittingState.gd
@@ -4,18 +4,19 @@ var orbital_parking_spot: Position3D = null
 
 func OnStateLoad(parameters: Array):
 	orbital_parking_spot = parameters[0]
-	
+
 func OnStateUnload():
+	LevelManager.release_orbit(orbital_parking_spot)
 	orbital_parking_spot = null
-	
+
 func Process(delta: float):
 	pass
-	
+
 func PhysicsProcess(delta: float):
 	target.global_transform.origin = orbital_parking_spot.global_transform.origin
-	
+
 func Draw():
 	pass
-	
+
 func Input(event: InputEvent):
 	pass
