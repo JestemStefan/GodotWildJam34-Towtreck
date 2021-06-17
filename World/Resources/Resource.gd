079befb723d17f5ae68b5e48e2630c0bc3244c9a
JestemStefan <pudipudi2x@gmail.com>
2021-06-17 19:09:31 +0200

    WIP: Hydrogen particle

--- World/Resources/Resource.gd
+++ World/Resources/Resource.gd
@@ -5,14 +5,26 @@ class_name GameResource
 export var color: Color
 export var resourceType: String
 
+enum ResourceType{HYDROGEN, ROCKS, ICE}
+export(ResourceType) var CloudType = ResourceType.HYDROGEN
+
 func _ready():
-	$OmniLight.light_color = color
 	add_to_group("resources")
 
 func areaEntered(body):
 	if body is Player:
 		body.ResourceAreaEntered(self)
 		
+		# I added it to save some performance. I heard HTML is bad with lighting
+		$OmniLight.show()
+		
 func areaExited(body):
 	if body is Player:
 		body.ResourceAreaExited(self)
+		
+		# I added it to save some performance. I heard HTML is bad with lighting
+		$OmniLight.hide()
+		
+
+func mine_resource():
+	pass
