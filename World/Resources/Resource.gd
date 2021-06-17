703ad0db0787c0c45f7855d88470fcf5d7ee4a39
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-06-17 23:49:21 +0200

    Added particle systems for cloud and gathering resources

--- World/Resources/Resource.gd
+++ World/Resources/Resource.gd
@@ -8,8 +8,32 @@ export var resourceType: String
 enum ResourceType{HYDROGEN, ROCKS, ICE}
 export(ResourceType) var CloudType = ResourceType.HYDROGEN
 
+onready var hydrogen_ps_cloud = preload("res://art/Particles/Hydrogen_Cloud_PS.tscn")
+onready var rock_ps_cloud = preload("res://art/Particles/Rock_Cloud_PS.tscn")
+onready var ice_ps_cloud = preload("res://art/Particles/Ice_Cloud_PS.tscn")
+
 func _ready():
 	add_to_group("resources")
+	
+	var cloud_particle_system: CPUParticles
+	var cloud_light_color: Color
+	
+	match CloudType:
+		ResourceType.HYDROGEN:
+			cloud_particle_system = hydrogen_ps_cloud.instance()
+			cloud_light_color = Color.red
+		
+		ResourceType.ROCKS:
+			cloud_particle_system = rock_ps_cloud.instance()
+			cloud_light_color = Color.orange
+		
+		ResourceType.ICE:
+			cloud_particle_system = ice_ps_cloud.instance()
+			cloud_light_color = Color.blue
+	
+	add_child(cloud_particle_system)
+	$OmniLight.light_color = cloud_light_color
+		
 
 func areaEntered(body):
 	if body is Player:
