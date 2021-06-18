921d3b348e4d34bd5877da48bef51ceb95d369c0
Grzegorz Puławski <pulawskig@gmail.com>
2021-06-18 23:48:24 +0200

    Added level creation

--- Menu/MainMenu.gd
+++ Menu/MainMenu.gd
@@ -5,7 +5,14 @@ func _ready():
 		$CenterContainer/VBoxContainer/QuitButton.hide()
 
 func _onStartButtonPressed():
-	get_tree().change_scene("res://World/TestEnvironment.tscn")
+#	get_tree().change_scene("res://World/TestEnvironment.tscn")
+	var planet1 = {
+		orbit = 50
+	}
+	var planet2 = {
+		orbit = 111
+	}
+	LevelManager.SetupLevel(Vector2(60, 60), [planet1, planet2], [Vector2(-60, -60)], [Vector2(60, -60)], [Vector2(-60, 60)])
 
 func _onOptionButtonPressed():
 	get_tree().change_scene("res://Menu/Options/Options.tscn")
--- World/Resources/Resource.gd
+++ World/Resources/Resource.gd
@@ -2,10 +2,7 @@ extends Spatial
 
 class_name GameResource
 
-export var color: Color
-export var resourceType: String
-
-enum ResourceType{HYDROGEN, ROCKS, ICE}
+enum ResourceType{ HYDROGEN = 1, ROCKS = 2, ICE = 3 }
 export(ResourceType) var CloudType = ResourceType.HYDROGEN
 
 onready var _hydrogen_ps_cloud = preload("res://art/Particles/Hydrogen_Cloud_PS.tscn")
@@ -18,9 +15,11 @@ onready var _ice_ps_cloud = preload("res://art/Particles/Ice_Cloud_PS.tscn")
 onready var _ice_gathering_ps = preload("res://art/Particles/Ice_Gathering.tscn")
 
 var gathering_ps
+var markerType
 
 func _ready():
 	add_to_group("resources")
+	add_to_group("minimap_targets")
 	
 	var cloud_particle_system: CPUParticles
 	var cloud_light_color: Color
@@ -30,16 +29,19 @@ func _ready():
 			cloud_particle_system = _hydrogen_ps_cloud.instance()
 			gathering_ps = _hydrogen_gathering_ps
 			cloud_light_color = Color.red
+			markerType = 5
 		
 		ResourceType.ROCKS:
 			cloud_particle_system = _rock_ps_cloud.instance()
 			gathering_ps = _rock_gathering_ps
 			cloud_light_color = Color.orange
+			markerType = 6
 		
 		ResourceType.ICE:
 			cloud_particle_system = _ice_ps_cloud.instance()
 			gathering_ps = _ice_gathering_ps
 			cloud_light_color = Color.blue
+			markerType = 7
 	
 	add_child(cloud_particle_system)
 	$OmniLight.light_color = cloud_light_color
